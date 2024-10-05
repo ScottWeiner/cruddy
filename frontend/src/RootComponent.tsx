cfa8f872204b7a1adfff3fcfb25e78cdabff3c06
Scott W <scottaza@gmail.com>
2024-10-05 12:33:14 -0700

    Initial for the FrontEnd Part.

---
+++ frontend/src/RootComponent.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import NotFoundPage from './pages/NotFoundPage'
+import { ROUTES } from './resources/routes-constants'
+import './styles/main.sass'
+import ProductPage from './pages/ProductPage'
+
+const RootComponent: React.FC = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="*" element={<NotFoundPage />} />
+                <Route path={ROUTES.HOMEPAGE_ROUTE} element={<HomePage />} />
+                <Route path={ROUTES.PRODUCT_ROUTE} element={<ProductPage />} />
+            </Routes>
+        </Router>
+    )
+}
+
+export default RootComponent
